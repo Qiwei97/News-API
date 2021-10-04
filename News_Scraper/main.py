bd20ab5b9fb691afc6a425e835cf2ec01d936427
Qiwei <lin_qiwei@u.nus.edu>
2021-10-04 16:35:14 +0800

    Change File Path

--- News_Scraper/main.py
+++ News_Scraper/main.py
@@ -7,11 +7,11 @@ app = FastAPI()
 
 @app.get("/")
 async def upload_news():
-    with open('data/news.json') as f:
+    with open('News_Scraper/data/news.json') as f:
         result = json.load(f)
     return result
 
 @repeat_every(seconds = 5 * 60)
 def update_news():
     df = generate_news(period='1M', category='Health')
-    df.to_json('data/news.json', orient='records')
+    df.to_json('News_Scraper/data/news.json', orient='records')
