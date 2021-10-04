39c5ed1f07e3cda9b5a9e3c067969ffb56852b93
Qiwei <lin_qiwei@u.nus.edu>
2021-10-04 17:18:22 +0800

    Update Endpoint

--- News_Scraper/main.py
+++ News_Scraper/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi_restful.tasks import repeat_every
 from News_Scraper.utils import generate_news
 import json
 
@@ -11,7 +10,8 @@ async def upload_news():
         result = json.load(f)
     return result
 
-@repeat_every(seconds = 5 * 60)
-def update_news():
+@app.get("/update_news/")
+async def update_news():
     df = generate_news(period='1M', category='Tech')
     df.to_json('News_Scraper/data/news.json', orient='records')
+    return('News Updated.')
