ce907efec303a83c4d8c39563ecd2c6e6f7bec7d
Qiwei <lin_qiwei@u.nus.edu>
2021-11-17 17:46:24 +0800

    Update Search Keywords

--- News_Scraper/main.py
+++ News_Scraper/main.py
@@ -12,6 +12,6 @@ async def upload_news():
 
 @app.get("/update_news/")
 async def update_news():
-    df = generate_news(period='1M', category='Tech')
+    df = generate_news(period='1M', category='health OR vaccine OR medicine OR disease OR hospital')
     df.to_json('News_Scraper/data/news.json', orient='records')
     return('News Updated.')
