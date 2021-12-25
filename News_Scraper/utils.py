3c4ecd6b48afc99916fd6e14cff308244d87dac7
Qiwei <lin_qiwei@u.nus.edu>
2021-12-25 19:23:32 +0800

    Update utils.py

--- News_Scraper/utils.py
+++ News_Scraper/utils.py
@@ -2,15 +2,14 @@ from GoogleNews import GoogleNews
 from newspaper import Article, Config
 import pandas as pd
 import time
-import pytz
 import numpy as np
 import nltk
 
 config = Config()
 config.MAX_SUMMARY_SENT = 2
-config.MIN_WORD_COUNT = 500
+config.MIN_WORD_COUNT = 200
 
-def generate_news(period='1M', category='Health'):
+def generate_news(period='3d', category='Health'):
 
     # Scrape from Google News
     print("Collecting Latest News...")
@@ -19,21 +18,14 @@ def generate_news(period='1M', category='Health'):
 
     for i in range(3):
         googlenews.get_page(i)
-        time.sleep(5)
+        time.sleep(2)
 
-    df = pd.DataFrame(googlenews.result())
+    df = pd.DataFrame(googlenews.result()).dropna()
     df = df.drop_duplicates(['title']).reset_index(drop = True)
-
-    df.date = df.date.str.extract('(\d+)').astype(int)
-    df.datetime = pd.to_datetime(df.datetime)
-
-    try:
-        df.loc[pd.isna(df.datetime), 'datetime'] = [pd.Timestamp.now(pytz.timezone('UTC')) - pd.DateOffset(minutes=i) for i in df.loc[pd.isna(df.datetime), 'date']]
-    except:
-        pass
-
+    df.datetime = pd.to_datetime(df.datetime).astype(str)
     df = df.drop(columns = ['date'])
-    df.datetime = pd.to_datetime(df.datetime, utc=True).dt.tz_convert('Asia/Singapore').astype(str)
+
+    assert len(df) > 0
 
     # Download Articles
     print("Downloading Articles...")
@@ -53,14 +45,16 @@ def generate_news(period='1M', category='Health'):
             article_dict['Summary'] = article.summary.replace('\n', ' ')
             article_dict['Image'] = article.top_image
             news.append(article_dict)
-            time.sleep(5)
+            time.sleep(3)
         except:
             pass
 
     news_df = pd.DataFrame(news)
+    # Remove erratic articles
+    news_df = news_df[news_df.Article.str.len() > 200]
     # Catch cases asking for sign up
     news_df.Summary = news_df.Summary.str.split('.').apply(lambda x: x.pop(0) if ('sign' in x[0]) or ('Sign' in x[0]) else x)
-    # Control word count
+    # Control summary word count
     news_df.Summary = news_df.Summary.str.join('.').str.split().apply(lambda x: x[:30]).str.join(' ') + '...'
     news_df.replace('', np.nan, inplace=True)
     news_df = news_df.dropna()
